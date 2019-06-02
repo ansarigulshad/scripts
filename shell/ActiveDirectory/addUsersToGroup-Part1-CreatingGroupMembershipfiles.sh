f7308ab255ecb22dc94f1647adfba9a8c75092d7
Gulshad Ansari <5801838+ansarigulshad@users.noreply.github.com>
2019-06-02 20:05:03 +0530

    Create addUsersToGroup-Part1-CreatingGroupMembershipfiles.sh

---
+++ shell/ActiveDirectory/addUsersToGroup-Part1-CreatingGroupMembershipfiles.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#################################################################
+# Script to create user group membership ldif file             ##
+# files will be stored under `groupmembers` directory          ##
+# members list should me added manually to all ldif file       ##
+#                                                              ##
+#                                                              ##
+#Author - Gulshad Ansari                                       ##
+#Email: gulshad.ansari@hotmail.com                             ##
+#LinkedIn : https://www.linkedin.com/in/gulshad/               ##
+#################################################################
+
+LOC=`pwd`
+AD_PROPETIES=ad.properties
+source $LOC/$AD_PROPETIES
+
+mkdir groupmembers
+
+for thisgroup in `cat groups.list`
+do 
+# touch groupmembers/$thisgroup"-members".list
+cat > groupmembers/$thisgroup"-members".ldif <<EOFILE
+dn: CN=$GROUPNAME,$ARG_GROUP_BASE
+changetype: modify
+add: member
+member: CN=Gulshad Ansari,$ARG_USER_BASE
+member: CN=Adnan Khan,$ARG_USER_BASE
+EOFILE
+done
+
+
+

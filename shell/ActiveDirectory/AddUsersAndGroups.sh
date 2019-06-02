b230d79e07e3e3c87e535a717253c97866878c5d
Gulshad Ansari <5801838+ansarigulshad@users.noreply.github.com>
2019-06-02 16:45:33 +0530

    Update AddUsersAndGroups.sh

--- shell/ActiveDirectory/AddUsersAndGroups.sh
+++ shell/ActiveDirectory/AddUsersAndGroups.sh
@@ -1,5 +1,58 @@
 #!/bin/bash
 
+########################################
+#Script to setup and configure MIT KDC##
+#Author - Gulshad Ansari			  ##
+########################################
+
+LOC=`pwd`
+AD_PROPETIES=ad.properties
+source $LOC/$AD_PROPETIES
+
+
+creat_ad_users()
+{
+  FIRSTNAME="$1"
+  LASTNAME="$2"
+
+  cat > /tmp/$FIRSTNAME.ldif <<EOFILE
+  dn: CN=$FIRSTNAME $LASTNAME,${ARG_SEARCHBASE}
+  changetype: add
+  objectClass: top
+  objectClass: person
+  objectClass: organizationalPerson
+  objectClass: user
+  cn: $FIRSTNAME $LASTNAME
+  sn: $FIRSTNAME$LASTNAME
+  givenName: $FIRSTNAME
+  displayName: $FIRSTNAME $LASTNAME
+  name: $FIRSTNAME $LASTNAME
+  accountExpires: 9223372036854775807
+  userAccountControl: 514
+  sAMAccountName: $FIRSTNAME$LASTNAME
+  userPrincipalName: $FIRSTNAME$LASTNAME@${ARG_DOMAIN}
+
+  dn: CN=$FIRSTNAME $LASTNAME,${ARG_SEARCHBASE}
+  changetype: modify
+  replace: unicodePwd
+  unicodePwd::${ARG_NewUserPass}
+
+  dn: CN=$FIRSTNAME $LASTNAME,${ARG_SEARCHBASE}
+  changetype: modify
+  replace: userAccountControl
+  userAccountControl: 512
+  EOFILE
+}
+
+
+while read LINE
+do
+  echo "Creating user: " $LINE
+	create_users $LINE
+done < $LOC/users.list
+
+
+
 
 
 

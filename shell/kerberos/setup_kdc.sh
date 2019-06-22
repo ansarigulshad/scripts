47a8f07457f60498ae863ac7c93be234a1d800a2
Gulshad Ansari <5801838+ansarigulshad@users.noreply.github.com>
2019-06-22 12:37:26 +0530

    Update setup_kdc.sh

--- shell/kerberos/setup_kdc.sh
+++ shell/kerberos/setup_kdc.sh
@@ -29,6 +29,7 @@ setup_kdc()
 	cat $LOC/krb5.conf.template > /etc/krb5.conf
 	echo -e "\n`currect_time_stamp` Configuring kdc.conf"
 	sed -i.bkp "s/EXAMPLE.COM/$REALM/g" /var/kerberos/krb5kdc/kdc.conf
+	#add renewabale life
 	sed -i.bkp'/{/a max_renewable_life = 7d 0h 0m 0s'  /var/kerberos/krb5kdc/kdc.conf
 	echo -e "\n`currect_time_stamp` Configuring kadm5.acl"
 	sed -i.bkp "s/EXAMPLE.COM/$REALM/g" /var/kerberos/krb5kdc/kadm5.acl

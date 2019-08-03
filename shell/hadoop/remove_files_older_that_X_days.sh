8ef4f165332d88ff5180ee5a05ab9b24a293049d
Gulshad Ansari <5801838+ansarigulshad@users.noreply.github.com>
2019-08-04 01:14:45 +0530

    Update remove_files_older_that_X_days.sh

--- shell/hadoop/remove_files_older_that_X_days.sh
+++ shell/hadoop/remove_files_older_that_X_days.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+#
+# Ref: https://community.hortonworks.com/questions/19204/do-we-have-any-script-which-we-can-use-to-clean-tm.html
+# 
 usage="Usage: dir_diff.sh [days]"
 hdfsPath=/ranger/audit/hdfs
 if [ ! "$1" ]
@@ -13,8 +17,10 @@ dir_date=`echo $f | awk '{print $6}'`
 difference=$(( ( $now - $(date -d "$dir_date" +%s) ) / (24 * 60 * 60 ) ))
   if [ $difference -gt $1 ]; 
   then
-    # write your remove commands here
     hadoop fs -ls `echo $f| awk '{ print $8 }'`;
+    
+    # replace above ls command with below rm command to remove data.
+    # hadoop fs -rm -r `echo $f| awk '{ print $8 }'`;
   fi
 done
 

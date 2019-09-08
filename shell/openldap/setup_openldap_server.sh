ecaa3cfff3c7f2c2d1475f90494408d3d79ba0f7
Gulshad Ansari <5801838+ansarigulshad@users.noreply.github.com>
2019-09-09 00:43:45 +0530

    Update setup_openldap_server.sh

--- shell/openldap/setup_openldap_server.sh
+++ shell/openldap/setup_openldap_server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #################################################################
 # Script to setup openLDAP server on centos/RHEL server        ##
@@ -10,29 +10,22 @@
 #LinkedIn : https://www.linkedin.com/in/gulshad/       	       ##
 #################################################################
 
-
-#LdapAdmin=ldapadmin
-#LdapAdminPassword="hadoop123"
-#MyPass=`slappasswd -s $LdapAdminPassword`
-#BaseDomain="dc=hortonworks,dc=com"
-#LdapAdminDN="cn=$LdapAdmin,$BaseDomain"
-
-
-LOC=`pwd`
-OPENLDAP_PROPETIES=openldap.properties
-source $LOC/$OPENLDAP_PROPETIES
-
-
+yum clean all -y
 yum -y install openldap-servers openldap-clients
 cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
 chown ldap. /var/lib/ldap/DB_CONFIG
 systemctl start slapd
 systemctl enable slapd
 
+
+LOC=`pwd`
+OPENLDAP_PROPETIES=openldap.properties
+source $LOC/$OPENLDAP_PROPETIES
+
 netstat -antup | grep -i 389
 
 
-echo -e "dn: olcDatabase={0}config,cn=config\n\nchangetype: modify\nadd: olcRootPW\nolcRootPW: $MyPass"  > /var/tmp/chrootpw.ldif
+echo -e "dn: olcDatabase={0}config,cn=config\n\nchangetype: modify\nadd: olcRootPW\nolcRootPW: $ARG_MyPass"  > /var/tmp/chrootpw.ldif
 
 
 ldapadd -Y EXTERNAL -H ldapi:/// -f /var/tmp/chrootpw.ldif
@@ -42,16 +35,23 @@ ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/nis.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif
 
-MyPass=`slappasswd -s $LdapAdminPassword`
+ARG_MyPass=`slappasswd -s $ARG_L_ADMINPASSWORD`
 
 
-echo -e "dn: olcDatabase={1}monitor,cn=config\nchangetype: modify\nreplace: olcAccess\nolcAccess: {0}to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth"\n  read by dn.base="$LdapAdminDN" read by * none\n\ndn: olcDatabase={2}hdb,cn=config\nchangetype: modify\nreplace: olcSuffix\nolcSuffix: $BaseDomain\n\ndn: olcDatabase={2}hdb,cn=config\nchangetype: modify\nreplace: olcRootDN\nolcRootDN: $LdapAdminDN\n\ndn: olcDatabase={2}hdb,cn=config\nchangetype: modify\nadd: olcRootPW\nolcRootPW: $MyPass\n\ndn: olcDatabase={2}hdb,cn=config\nchangetype: modify\nadd: olcAccess\nolcAccess: {0}to attrs=userPassword,shadowLastChange by\n dn="$LdapAdminDN" write by anonymous auth by self write by * none\nolcAccess: {1}to dn.base="" by * read\nolcAccess: {2}to * by dn="$LdapAdminDN" write by * read\n" > /var/tmp/chdomain.ldif
+echo -e "dn: olcDatabase={1}monitor,cn=config\nchangetype: modify\nreplace: olcAccess\nolcAccess: {0}to * by dn.base="gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth"\n  read by dn.base="$ARG_BINDDN" read by * none\n\ndn: olcDatabase={2}hdb,cn=config\nchangetype: modify\nreplace: olcSuffix\nolcSuffix: $ARG_DOMAINCONTROLLER\n\ndn: olcDatabase={2}hdb,cn=config\nchangetype: modify\nreplace: olcRootDN\nolcRootDN: $ARG_BINDDN\n\ndn: olcDatabase={2}hdb,cn=config\nchangetype: modify\nadd: olcRootPW\nolcRootPW: $ARG_MyPass\n\ndn: olcDatabase={2}hdb,cn=config\nchangetype: modify\nadd: olcAccess\nolcAccess: {0}to attrs=userPassword,shadowLastChange by\n dn="$ARG_BINDDN" write by anonymous auth by self write by * none\nolcAccess: {1}to dn.base="" by * read\nolcAccess: {2}to * by dn="$ARG_BINDDN" write by * read\n" > /var/tmp/chdomain.ldif
 
 
 ldapmodify -Y EXTERNAL -H ldapi:/// -f /var/tmp/chdomain.ldif
 
-echo -e "\ndn: $BaseDomain\nobjectClass: top\nobjectClass: dcObject\nobjectclass: organization\no: `echo $BaseDomain | cut -d ',' -f1 | cut -d '=' -f2`\ndc: `echo $BaseDomain | cut -d ',' -f1 | cut -d '=' -f2`\n\ndn: $LdapAdminDN\nobjectClass: organizationalRole\ncn: Manager\ndescription: Directory Manager\n\ndn: ou=People,$BaseDomain\nobjectClass: organizationalUnit\nou: People\n\ndn: ou=Group,$BaseDomain\nobjectClass: organizationalUnit\nou: Group\n\ndn: ou=Users,$BaseDomain\nobjectClass: organizationalUnit\nou: Users\n\ndn: ou=Hadoop,$BaseDomain\nobjectClass: organizationalUnit\nou: Hadoop\ndescription: organizationalUnit for BigData & Hadoop" > /var/tmp/basedomain.ldif
+#echo -e "\ndn: $ARG_DOMAINCONTROLLER\nobjectClass: top\nobjectClass: dcObject\nobjectclass: organization\no: `echo $ARG_DOMAINCONTROLLER | cut -d ',' -f1 | cut -d '=' -f2`\ndc: `echo $ARG_DOMAINCONTROLLER | cut -d ',' -f1 | cut -d '=' -f2`\n\ndn: $ARG_BINDDN\nobjectClass: organizationalRole\ncn: ldapadmin\ndescription: Directory ldapadmin\n\ndn: ou=People,$ARG_DOMAINCONTROLLER\nobjectClass: organizationalUnit\nou: People\n\ndn: ou=Group,$ARG_DOMAINCONTROLLER\nobjectClass: organizationalUnit\nou: Group\n\ndn: ou=Users,$ARG_DOMAINCONTROLLER\nobjectClass: organizationalUnit\nou: Users\n\ndn: ou=Hadoop,$ARG_DOMAINCONTROLLER\nobjectClass: organizationalUnit\nou: Hadoop\ndescription: organizationalUnit for BigData & Hadoop" > /var/tmp/ARG_DOMAINCONTROLLER.ldif
+
+
+echo -e "\ndn: $ARG_DOMAINCONTROLLER\nobjectClass: top\nobjectClass: dcObject\nobjectclass: organization\no: `echo $ARG_DOMAINCONTROLLER | cut -d ',' -f1 | cut -d '=' -f2`\ndc: `echo $ARG_DOMAINCONTROLLER | cut -d ',' -f1 | cut -d '=' -f2`\n\ndn: $ARG_BINDDN\nobjectClass: organizationalRole\ncn: ldapadmin\ndescription: Directory ldapadmin\n\ndn: ou=People,$ARG_DOMAINCONTROLLER\nobjectClass: organizationalUnit\nou: People\n\ndn: ou=Group,$ARG_DOMAINCONTROLLER\nobjectClass: organizationalUnit\nou: Group\n\ndn: ou=Users,$ARG_DOMAINCONTROLLER\nobjectClass: organizationalUnit\nou: Users\n\ndn: ou=Hadoop,$ARG_DOMAINCONTROLLER\nobjectClass: organizationalUnit\nou: Hadoop\ndescription: organizationalUnit for BigData & Hadoop\n\ndn: ou=Users,ou=Hadoop,$ARG_DOMAINCONTROLLER\nobjectClass: organizationalUnit\nou: Hadoop Users\ndescription: organizationalUnit for BigData & Hadoop Users\n\ndn: ou=Groups,ou=Hadoop,$ARG_DOMAINCONTROLLER\nobjectClass: organizationalUnit\nou: Hadoop Groups\ndescription: organizationalUnit for BigData & Hadoop Groups" > /var/tmp/ARG_DOMAINCONTROLLER.ldif
+
+
+ldapadd -x -D $ARG_BINDDN -w $ARG_L_ADMINPASSWORD -f /var/tmp/ARG_DOMAINCONTROLLER.ldif
 
 
 
-ldapadd -x -D $LdapAdminDN -w $LdapAdminPassword -f /var/tmp/basedomain.ldif
+
+# done

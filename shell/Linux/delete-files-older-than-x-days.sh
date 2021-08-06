82fedcaad6feb60b183b9d024a3b452da9e2bd44
Gulshad Ansari <5801838+ansarigulshad@users.noreply.github.com>
2021-08-06 12:37:22 +0530

    Create delete-files-older-than-x-days.sh

---
+++ shell/Linux/delete-files-older-than-x-days.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Find Nifi logs older than 2 days and create tar
+find /var/log/hadoop/nifi/ -type f -mtime +2 | xargs tar czf /hadoop/nifi-logs-archive/nifi-logs-archive-date-`date +%d-%m-%y`.tar.gz
+
+# Delete nifi logs older than 2 days
+find /var/log/hadoop/nifi/ -mtime +2 -exec rm {} \;
+
+# Delete nifi logs older than 10 days
+find /hadoop/nifi-logs-archive/ -mtime +10 -exec rm {} \;
+
+
+
+
